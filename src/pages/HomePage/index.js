595ca685c9d90ec7b4c740c524dc91fe1f73ee0f
Bobinoza <marioguerra.motorolla@gmail.com>
2021-10-24 15:17:24 -0300

    first commit

---
+++ src/pages/HomePage/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Navbar from '../../components/NavBar/Navbar.js';
+
+
+function Home() {
+    return(
+        <>
+            <Navbar />
+        </>
+        
+        
+    );
+}
+
+export default Home;
